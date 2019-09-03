2b04d19607eb044e7b40573eeac1f3d517fb89ec
Dmitry Marokhonov <dmitry.marokhonov@cybercom.com>
2019-09-03 09:00:57 +0300

    382 Adding Pagination

--- controllers/feed.mjs
+++ controllers/feed.mjs
@@ -9,11 +9,26 @@ import Post from "../models/post";
 import { __dirname } from "../app";
 
 export const getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
+
   Post.find()
+    .countDocuments()
+    .then(count => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then(posts => {
       res
         .status(200)
-        .json({ message: "Fetched posts successfully.", posts: posts });
+        .json({
+          message: "Fetched posts successfully.",
+          posts: posts,
+          totalItems: totalItems
+        });
     })
     .catch(err => {
       if (!err.statusCode) {
@@ -135,17 +150,18 @@ export const deletePost = (req, res, next) => {
 
   Post.findById(postId)
     .then(post => {
-      if(!post) {
-        const error = new Error("Could not find a post.")
+      if (!post) {
+        const error = new Error("Could not find a post.");
         error.statusCode = 404;
         throw error;
       }
       // Check logged in user here
       clearImage(post.imageUrl);
       return Post.findByIdAndRemove(postId);
-    }).then(result => {
-      console.log(result)
-      res.status(200).json({message: "Deleted post."})
+    })
+    .then(result => {
+      console.log(result);
+      res.status(200).json({ message: "Deleted post." });
     })
     .catch(err => {
       if (!err.statusCode) {
